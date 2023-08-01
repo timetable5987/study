6dced247ee9ab53a4ab1d1235af0a6f05a431e54
timetable5678 <coolcats6762@gmail.com>
2023-08-01 21:04:30 +0900

    0801

---
+++ 코딩테스트/프로그래머스 Lv 1/프로그래머스-문자열 나누기/src/main.java
@@ -0,0 +1,60 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/140108
+
+문자열 s가 입력되었을 때 다음 규칙을 따라서 이 문자열을 여러 문자열로 분해하려고 합니다.
+
+먼저 첫 글자를 읽습니다. 이 글자를 x라고 합시다.
+이제 이 문자열을 왼쪽에서 오른쪽으로 읽어나가면서, x와 x가 아닌 다른 글자들이 나온 횟수를 각각 셉니다. 처음으로 두 횟수가 같아지는 순간 멈추고, 지금까지 읽은 문자열을 분리합니다.
+s에서 분리한 문자열을 빼고 남은 부분에 대해서 이 과정을 반복합니다. 남은 부분이 없다면 종료합니다.
+만약 두 횟수가 다른 상태에서 더 이상 읽을 글자가 없다면, 역시 지금까지 읽은 문자열을 분리하고, 종료합니다.
+문자열 s가 매개변수로 주어질 때, 위 과정과 같이 문자열들로 분해하고, 
+분해한 문자열의 개수를 return 하는 함수 solution을 완성하세요.
+*/
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String first = String.valueOf(s.charAt(0));
+        
+        while(true){
+            
+            try{
+                int n = cut(String.valueOf(s.charAt(0)),s);  
+                if(n == 0){
+                    answer++;
+                    break;
+                 }           
+                
+                s = s.substring(n);    
+                answer++;
+
+            }catch(StringIndexOutOfBoundsException e){
+            //    System.out.println("오류 : "+ s + " , " + answer);
+                break;
+            }            
+           // System.out.println(s + " , " + answer);           
+        }     
+        
+        return answer;
+    }
+    
+    public int cut(String first, String s){
+        
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(String.valueOf(s.charAt(i)).equals(first)){
+                x++;
+            }else{
+                y++;
+            }
+            
+            if(x != 0 && x == y){
+                return i+1;
+            }
+            
+        }
+        return 0;
+    }
+    
+}
